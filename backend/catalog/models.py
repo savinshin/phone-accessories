7ee31b507cb78d279fd24399fe251e5e77ec3582
Savin Ilya <savinbelilya@gmail.com>
2026-09-21 20:22:37 +0200

    Revert "Catalog DB — 1. Category + Brand (#1)" (#5)
    This reverts commit a71316fd56ca549918498908673e73944d64735a.

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,26 +1,3 @@
 from django.db import models
 
-
-class Category(models.Model):
-    parent = models.ForeignKey("Category",null=True, blank=True, on_delete = models.PROTECT, related_name="children" )
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
-    is_active = models.BooleanField(default=True)
-    sort_order = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-class Brand(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-            return self.name
+# Create your models here.
