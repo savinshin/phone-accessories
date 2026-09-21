a71316fd56ca549918498908673e73944d64735a
prokurat33 <prokurat33@gmail.com>
2026-09-21 21:12:47 +0300

    Catalog DB — 1. Category + Brand (#1)

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey("Category",null=True, blank=True, on_delete = models.PROTECT, related_name="children" )
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    sort_order = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+            return self.name
